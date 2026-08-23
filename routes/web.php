ac764ffee81e8331cb2846c495b7d45f989248c9
Menelaos Georgiadis <kemeweb@gmail.com>
2026-08-23 17:18:15 +0300

    Change on master branch

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// My first Git change
-// Change made in test branch
-// Change made in test branch
+// Change made on master
